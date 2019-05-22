93d2e205e2f78094aba170cdb2381c727d0d53bb
root <root@master.image.test>
2019-05-22 16:35:27 +0200

    commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+  return 'Hello'
+ 
+if __name__ == '__main__':
+    app.secret_key = 'Qm9nZGEgbWUgbzUgemV6YQ=='
+    app.run(host='0.0.0.0',
+            port=5000)
