c10b8a1818fb7f30136a99ca6407dc69b2e74aab
uzugic <42067801+uzugic@users.noreply.github.com>
2019-06-05 14:16:14 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup  
-setup(name='ws',
-    version='1.0',
-    description='hello world hello mica app',
-    author='micacar',
-    author_email='mica@car.com',
-    url='http://www.python.org/sigs/distutils-sig/',  
-    install_requires=['Flask','flask-wtf','flask-babel','markdown','flup'],
-)
